f8f414646349fded18ecf7c7fb192c0635682162
DongHyun0521 <132866563+DongHyun0521@users.noreply.github.com>
2023-11-28 17:07:34 +0900

    Update library.js

--- library.js
+++ library.js
@@ -20,9 +20,9 @@
 		if (data.postData.content.match(regularUrl)) {
 			data.postData.content = data.postData.content.replace(regularUrl, embed);
 		}
-		if (data.postData.content.match(shortUrl)) {
-			data.postData.content = data.postData.content.replace(shortUrl, embed);
-		}
+		//if (data.postData.content.match(shortUrl)) {
+		//	data.postData.content = data.postData.content.replace(shortUrl, embed);
+		//}
 
 		callback(null, data);
 	};
